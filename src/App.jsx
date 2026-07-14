fa93bba356ff723f1ba06a42fcf14cf60eced182
Ref-DevSa <ref.devsa@gmail.com>
2026-07-14 09:29:01 +0200

    Fetch users from API using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="container">
       <h1>User Directory</h1>
+
+      <p>Number of users: {users.length}</p>
     </div>
   );
 }
